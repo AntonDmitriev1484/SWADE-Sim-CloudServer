7cd8fdee1e84d6d02724c21901cb9d3e7cac0e6c
AntonDmitriev1484 <a.dmitriev@wustl.edu>
2023-07-18 13:26:05 -0700

    Fetch HOF Fetch HOF :o

--- src/index.js
+++ src/index.js
@@ -1,66 +1,31 @@
 import express from "express"
-import moment from "moment"
-import fetch from "node-fetch"
+import  * as f from "./fetch_methods.js"
 
 const app = express();
 app.use(express.json());
 
-// Data for testing put requests
-const data = ["1", "2", "buckle my shoe", "3", "4", "buckle some more"]
+function heartbeat() {
+    const timer = 1000*30; //30s
+    const endpoint = "http://e-srv:3000/sync";
 
-function test_get() {
-    const endpoint = "http://s3:9000/test/Anime.txt"
-    fetch(endpoint, {
-        method: 'GET',
-        headers: {
-            'Host':'s3:9000',
-            'x-amz-date': moment().toISOString(),
-            'Authorization':'AKIAIOSFODNN7EXAMPLE',
-            'Content-Type': 'application/json'
-        }
-    })
-    .then(response => {
-        console.log(response.json())
-        return response.json()
-    })
-    .then(result => {
-        console.log('GET request successful');
-        console.log(result);
-      })
-    .catch(error => {
-        console.error('Error:', error);
-      });
-}
-
-
-// The API is receiving this requests, at the correct endpoint, just not really
-// doing anything with them at the moment.
-// Figure out how I would set up a table with this.
-function test_put() {
-    const endpoint = "http://s3:9000/test/Anime.txt"
-    data.map(
-        (x) => {
-            fetch(endpoint, {
-                method: 'PUT',
+    setInterval(() => {
+        f.HOFetch(
+            endpoint, 
+            {
+                method: 'GET',
                 headers: {
-                    'Host':'s3:9000',
-                    'x-amz-date': moment().toISOString(),
-                    'Authorization':'AKIAIOSFODNN7EXAMPLE',
                     'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(x)
-            })
-            .then(response => {response.json()})
-            .then(result => {
-                console.log('POST request successful');
-                console.log(result);
-              })
-            .catch(error => {
-                console.error('Error:', error);
-              });
-        }
-    )
+                }
+            },
+            (response) => {
+                // Update database using response
+            }
+        )
+        
+        }, 
+    timer);
 }
+
 
 
 app.post('/', (req, res)=> {
@@ -71,5 +36,5 @@ app.post('/', (req, res)=> {
 })
 
 app.listen(3000, () => {
-    test_put();
+    f.test_put();
 })
