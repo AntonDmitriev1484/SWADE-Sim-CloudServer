0c6992a8904269d57f5fbecc8510d6bea6c4d3c3
AntonDmitriev1484 <a.dmitriev@wustl.edu>
2023-07-18 11:15:06 -0700

    Post request doesn't do anything but connects :)

--- src/index.js
+++ src/index.js
@@ -5,64 +5,63 @@ import fetch from "node-fetch"
 const app = express();
 app.use(express.json());
 
+// Data for testing put requests
 const data = ["1", "2", "buckle my shoe", "3", "4", "buckle some more"]
-const endpoint = "http://s3:9000/test/Anime.txt"
 
-//Run S3 API-Calls against http://localhost:9444/ (e.g. http://localhost:9444/test-bucket/test-object)
-//http://localhost:9444/ui
-
-fetch(endpoint, {
-    method: 'GET',
-    headers: {
-        'Host':'s3:9000',
-        'x-amz-date': moment().toISOString(),
-        'Authorization':'AKIAIOSFODNN7EXAMPLE',
-        'Content-Type': 'application/json'
-    }
-})
-.then(response => {
-    console.log(response.json())
-    return response.json()
-})
-.then(result => {
-    console.log('POST request successful');
-    console.log(result);
-  })
-.catch(error => {
-    console.error('Error:', error);
-  });
-
+function test_get() {
+    const endpoint = "http://s3:9000/test/Anime.txt"
+    fetch(endpoint, {
+        method: 'GET',
+        headers: {
+            'Host':'s3:9000',
+            'x-amz-date': moment().toISOString(),
+            'Authorization':'AKIAIOSFODNN7EXAMPLE',
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => {
+        console.log(response.json())
+        return response.json()
+    })
+    .then(result => {
+        console.log('GET request successful');
+        console.log(result);
+      })
+    .catch(error => {
+        console.error('Error:', error);
+      });
+}
 
 
-//   method: 'GET',
-//     headers: {
-//         'Authorization':'AKIAIOSFODNN7EXAMPLE',
-//         'Content-Type': 'application/json'
-//     }
+// The API is receiving this requests, at the correct endpoint, just not really
+// doing anything with them at the moment.
+// Figure out how I would set up a table with this.
+function test_put() {
+    const endpoint = "http://s3:9000/test/Anime.txt"
+    data.map(
+        (x) => {
+            fetch(endpoint, {
+                method: 'PUT',
+                headers: {
+                    'Host':'s3:9000',
+                    'x-amz-date': moment().toISOString(),
+                    'Authorization':'AKIAIOSFODNN7EXAMPLE',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(x)
+            })
+            .then(response => {response.json()})
+            .then(result => {
+                console.log('POST request successful');
+                console.log(result);
+              })
+            .catch(error => {
+                console.error('Error:', error);
+              });
+        }
+    )
+}
 
-// data.map(
-//     (x) => {
-//         fetch(endpoint, {
-//             method: 'PUT',
-//             headers: {
-//                 'aws_access_key_id':'AKIAIOSFODNN7EXAMPLE',
-//                 'Content-Type': 'application/json'
-//             },
-//             body: JSON.stringify(x)
-//         })
-//         .then(response => {
-//             console.log(response.json())
-//             return response.json()
-//         })
-//         .then(result => {
-//             console.log('POST request successful');
-//             console.log(result);
-//           })
-//         .catch(error => {
-//             console.error('Error:', error);
-//           });
-//     }
-// )
 
 app.post('/', (req, res)=> {
     console.log("Received POST request:")
@@ -72,5 +71,5 @@ app.post('/', (req, res)=> {
 })
 
 app.listen(3000, () => {
-    console.log("Listening for requests on 3000");
+    test_put();
 })
